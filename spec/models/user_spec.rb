73a673eaa068241c9517c786804a092ad65b1a59
ryuseikorogi <70619565+ryuseikorogi@users.noreply.github.com>
2020-10-06 19:40:20 +0900

    姓、名、姓（カタカナ）名（カタカナ）)が空の場合登録できないことの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,5 +49,25 @@ RSpec.describe User, type: :model do
         expect(@user).to_not be_valid
         expect(@user.errors[:email]).to include("is invalid")
     end
+    it "family_nameが空だと登録できない" do
+      @user.family_name =""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name can't be blank")
+    end
+    it "first_nameが空だと登録できない" do
+      @user.first_name =""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+   end
+    it "family_name_kanaが空だと登録できない" do
+      @user.family_name_kana =""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name kana can't be blank")
+   end
+    it "first_name_kanaが空だと登録できない" do
+      @user.first_name_kana =""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
+   end
   end
 end
