2402171cdbeef0a317d848a6343018ab4c39eeae
ryuseikorogi <70619565+ryuseikorogi@users.noreply.github.com>
2020-10-12 17:03:15 +0900

    こちらはログインしたユーザーのみ出品画面に遷移できる様にする記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update]
+  before_action :move_to_index, except: [:index, :show]
   # def index 
   #   @items = Item.order("created_at DESC")
   # end
