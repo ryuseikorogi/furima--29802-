3e458d0495ff287058b4b6aa284e5c3d11209573
ryuseikorogi <70619565+ryuseikorogi@users.noreply.github.com>
2020-10-12 18:32:30 +0900

    商品一覧機能実装

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,6 +3,3 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
-
-// packのディレクトリでファイル作成をしましょう。price.js
-// ①入力された値を取得して変数に入れる　https://www.sejuku.net/blog/27019　https://master.tech-camp.in/curriculums/4672
