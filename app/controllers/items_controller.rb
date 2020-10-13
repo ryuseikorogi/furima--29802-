b6b0d1813fde9f5050aac1f2b6fc135a2dc976b7
ryuseikorogi <70619565+ryuseikorogi@users.noreply.github.com>
2020-10-13 20:31:44 +0900

    商品情報をログインしたユーザーのみ削除できる様な記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :delete]
   def index 
     @items = Item.order("created_at DESC")
   end
@@ -13,6 +13,15 @@ class ItemsController < ApplicationController
     redirect_to item_path
   else
     render :edit
+  end
+end
+
+def destroy
+  @item = Item.find(params[:id])
+  if @item.destroy
+  redirect_to root_path
+  else
+    render :index
   end
 end
 
