28cf8d715d718a6569e4d1f72527e71eeb82f59a
ryuseikorogi <70619565+ryuseikorogi@users.noreply.github.com>
2020-10-16 17:38:17 +0900

    変数名の変更

--- app/models/pay_form.rb
+++ app/models/pay_form.rb
@@ -15,8 +15,8 @@ class PayForm
   # ここにバリデーションの処理を書く
 
   def save
-    test = Purchase.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(shipping_prefecture_id: shipping_prefecture_id, address: address, zip_code: zip_code, city: city, building: building, phone_number: phone_number, purchase_id: test.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(shipping_prefecture_id: shipping_prefecture_id, address: address, zip_code: zip_code, city: city, building: building, phone_number: phone_number, purchase_id: purchase.id)
 
     # 各テーブルにデータを保存する処理を書く
   end
