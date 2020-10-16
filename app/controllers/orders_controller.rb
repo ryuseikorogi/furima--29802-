5b0011407773e027f8268eec3dde86f290a615aa
ryuseikorogi <70619565+ryuseikorogi@users.noreply.github.com>
2020-10-16 16:38:17 +0900

    endの位置を修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,48 +1,42 @@
 class OrdersController < ApplicationController
-  before_action :pay_form, only:[:index, :create]
+  before_action :pay_form, only: [:index, :create]
   before_action :authenticate_user!, only: [:index]
 
   def index
-    if @item.purchase
-      return ridirect_to root_path
-    end
-    if current_user == @item.user
-      redirect_to root_path
-    end
+    return ridirect_to root_path if @item.purchase
+
+    redirect_to root_path if current_user == @item.user
     @order = PayForm.new
   end
 
   def create
     @order = PayForm.new(order_params)
-  
-   if @order.valid?
+
+    if @order.valid?
       pay_item
       @order.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render 'index'
-    end
+     end
   end
 
+    private
 
-
-   private
-   def pay_form
-   @item = Item.find(params[:item_id])
-   end
-
-    def pay_item
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount:@item.price,
-        card: order_params[:token],
-        currency:'jpy'
-      )
-    end
-
-    def order_params
-      params.require(:pay_form).permit(:shipping_prefecture_id, :address, :zip_code, :city, :building, :phone_number).merge(token: params[:token], user_id: current_user.id ,item_id: params[:item_id])
-    end
+  def pay_form
+    @item = Item.find(params[:item_id])
   end
 
-  
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def order_params
+    params.require(:pay_form).permit(:shipping_prefecture_id, :address, :zip_code, :city, :building, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+  end
